13053a6e6b711d3f540373c2f37aae7fbd46595f
z4na14 <zanajorgesaghin@gmail.com>
2025-03-25 13:16:26 +0100

    Me he tocado cosas

--- Makefile
+++ Makefile
@@ -6,14 +6,20 @@ BUILD_DIR = ./build
 SRCS = $(wildcard $(SRC_DIR)/*.c)
 OBJS = $(patsubst $(SRC_DIR)/%.c, $(BUILD_DIR)/%.o, $(SRCS))
 
-all: $(BUILD_DIR)/mygrep $(BUILD_DIR)/scripter
+all: mygrepbld scripterbld
 
 $(BUILD_DIR)/%.o: $(SRC_DIR)/%.c
 	@mkdir -p $(BUILD_DIR)
 	$(CC) $(CFLAGS) -c $< -o $@
 
+.PHONY: mygrepbld
+mygrepbld: $(BUILD_DIR)/mygrep
+
 $(BUILD_DIR)/mygrep:  $(BUILD_DIR)/mygrep.o
 	$(CC) $(CFLAGS) $^ -o $@
+
+.PHONY: scripterbld
+scripterbld: $(BUILD_DIR)/scripter
 
 $(BUILD_DIR)/scripter: $(BUILD_DIR)/scripter.o
 	$(CC) $(CFLAGS) $^ -o $@
--- src/scripter.c
+++ src/scripter.c
@@ -15,14 +15,14 @@ const int max_commands = 10;
 /* VARS TO BE USED FOR THE STUDENTS */
 char* argvv[max_args];
 char* filev[max_redirections];
-int background = 0; 
+int background = 0;
 
 
 
 /**
  * This function splits a char* line into different tokens based on a given character
  * @return Number of tokens 
- *//*
+ */
 int tokenizar_linea(char *linea, char *delim, char *tokens[], int max_tokens) {
     int i = 0;
     char *token = strtok(linea, delim);
@@ -32,7 +32,8 @@ int tokenizar_linea(char *linea, char *delim, char *tokens[], int max_tokens) {
     }
     tokens[i] = NULL;
     return i;
-}*/
+}
+
 
 /**
  * This function processes the command line to evaluate if there are redirections. 
@@ -40,7 +41,7 @@ int tokenizar_linea(char *linea, char *delim, char *tokens[], int max_tokens) {
  * filev[0] for STDIN
  * filev[1] for STDOUT
  * filev[2] for STDERR
- *//*
+ */
 void procesar_redirecciones(char *args[]) {
     //initialization for every command
     filev[0] = NULL;
@@ -63,7 +64,8 @@ void procesar_redirecciones(char *args[]) {
             args[i + 1] = NULL;
         }
     }
-}*/
+}
+
 
 /**
  * This function processes the input command line and returns in global variables: 
@@ -71,8 +73,6 @@ void procesar_redirecciones(char *args[]) {
  * filev -- files for redirections. NULL value means no redirection. 
  * background -- 0 means foreground; 1 background.
  */
-
-/*
 int procesar_linea(char *linea) {
     char *comandos[max_commands];
     int num_comandos = tokenizar_linea(linea, "|", comandos, max_commands);
@@ -88,7 +88,7 @@ int procesar_linea(char *linea) {
     //Finish processing
     for (int i = 0; i < num_comandos; i++) {
         int args_count = tokenizar_linea(comandos[i], " \t\n", argvv, max_args);
-        procesar_redirecciones(argvv);*/
+        procesar_redirecciones(argvv);
 
         /********* This piece of code prints the command, args, redirections and background. **********/
         /*********************** REMOVE BEFORE THE SUBMISSION *****************************************/
@@ -106,17 +106,17 @@ int procesar_linea(char *linea) {
         if(filev[2] != NULL)
             printf("Redir [ERR] = %s\n", filev[2]);*/
         /**********************************************************************************************/
-    /*}
+    }
 
     return num_comandos;
-}*/
+}
 
 
 int main(int argc, char *argv[]) {
 
-    /* STUDENTS CODE MUST BE HERE *//*
+    /* STUDENTS CODE MUST BE HERE */
     char example_line[] = "ls -l | grep scripter | wc -l > redir_out.txt &";
-    int n_commands = procesar_linea(example_line);*/
+    int n_commands = procesar_linea(example_line);
 
     printf("HOLA MUNDO");
     
